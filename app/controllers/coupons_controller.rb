e6300fc2ed71123fb7a6f4a3753e9025f52fec7a
iwilliams83 <indirajamurath@gmail.com>
2018-07-01 16:15:04 +0000

    automatically backed up by learn

---
+++ app/controllers/coupons_controller.rb
@@ -0,0 +1,20 @@
+class CouponsController < ApplicationController
+  def index
+    @coupons = Coupon.all
+    render :index
+  end
+
+  def new
+    render :new
+  end
+
+  def show
+    @coupon = Coupon.find(params[:id])
+    render :show
+  end
+
+  def create
+    @coupon = Coupon.create(coupon_code: params[:coupon_code], store: params[:store])
+    redirect_to coupon_path(@coupon)
+  end
+end
